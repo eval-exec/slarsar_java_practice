1e02dad3d1da8cbb6f1037584712833fcd7815c7
slarsar <slarsar@yandex.com>
2019-02-14 16:43:13 +0800

    add pricechoisce

---
+++ java_practic/javahard/test2/pricechoice.java
@@ -0,0 +1,8 @@
+public class pricechoice{
+	public static void main(String[] args){
+		System.out.println("println");
+		System.out.println();
+		System.out.print("print");
+//		System.out.print();
+	}
+}

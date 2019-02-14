2aa3d13fbfc82075df5b2d647580cf24cdf29465
slarsar <slarsar@yandex.com>
2019-02-14 17:56:52 +0800

    modified two files

--- java_practic/javahard/test3/escapeandcomments.java
+++ java_practic/javahard/test3/escapeandcomments.java
@@ -1,5 +1,9 @@
 public class escapeandcomments{
 	public static void main(String[] args){
 		System.out.print("this is a  aaaaaa a a a a !\n");
+		System.out.println("aaaaaaaa\\aaaaa\\aaaa");
+		System.out.println("\\\\\\\\\\\\\\\\\\");
+		System.out.println("//\\// eawfeaw//\\//");
+		System.out.println("fewafeaw");
 	}
 }

8e3c4fabfdbb7aadb62d8b456b8b6d7ffac4e9f7
slarsar <slarsar@yandex.com>
2019-02-14 16:33:40 +0800

    add gasoline java file

---
+++ java_practic/javahard/test1/gasoline.java
@@ -0,0 +1,19 @@
+public class gasoline {
+	public static void main(String[] args){
+		System.out.println("|-------------------|");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|                   |");
+		System.out.println("|-------------------|");
+	}
+}
+

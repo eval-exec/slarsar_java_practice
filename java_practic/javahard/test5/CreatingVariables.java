67d3befc8a6acbba981dd641ca261d785e27bc7f
slarsar <slarsar@yandex.com>
2019-02-15 18:18:23 +0800

    deleted java_intelli

---
+++ java_practic/javahard/test5/CreatingVariables.java
@@ -0,0 +1,17 @@
+public class CreatingVariables{
+	public static void main(String[] args){
+		int x,y,z;
+		double a,b,c;
+		String firstname,lastname;
+		x = 10;
+		y = 20;
+		z = 30;
+		a = 3.1415269;
+		b = 1.73;
+		c = 1.222222;
+		firstname = "gram";
+		lastname = "vory";
+		System.out.println("my name is"+firstname+lastname+"this is my name!");
+		System.out.println(" these are some numbers"+x+y+z+"these number dis played ara some number s that can be dis played on the computer correctly");
+	}
+}
